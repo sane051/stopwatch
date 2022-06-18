1f70e142f2abf467ce4b7e1cfe458c885041f979
sane051 <99936825+sane051@users.noreply.github.com>
2022-06-18 20:28:12 +0900

    Update Untitled2.js

--- Untitled2.js
+++ Untitled2.js
@@ -30,87 +30,86 @@
 
 
 
-    //Date.now()はタイムスタンプを返す。タイムスタンプは、UTCでの1970年1月1日0時0分0秒から現在までの経過時間をミリ秒単位で表したもの。
     
-    
-    function updateTimetText(){ 
+   　//Date.now()はタイムスタンプを返す。タイムスタンプは、UTCでの1970年1月1日0時0分0秒から現在までの経過時間をミリ秒単位で表したもの。
+   　function updateTimetText(){ 
       
-      //60分は60000マイクロ秒＊60。
-       let mm = Math.floor(elapsedTime /3600000);
-      //1分は60000マイクロ秒。60000で割ることにより、分が計算される。
-       let m = Math.floor(elapsedTime /60000);
+     　　 //60分は60000マイクロ秒＊60。
+       　　let mm = Math.floor(elapsedTime /　3600000);
+      　　//1分は60000マイクロ秒。60000で割ることにより、分が計算される。
+      　　 let m = Math.floor(elapsedTime /　60000);
        
-      //1分は60000マイクロ秒。60000ミリ秒で割り、その余りを1000で割れば秒が計算される。
-       let s = Math.floor((elapsedTime % 60000) / 1000);
+      　　//1分は60000マイクロ秒。60000ミリ秒で割り、その余りを1000で割れば秒が計算される。
+      　　 let s = Math.floor((elapsedTime % 60000) / 1000);
        
-       let ms = elapsedTime % 1000;
+      　　 let ms = elapsedTime % 1000;
        
-    //桁数を合わせるため、０で埋めていく
-       mm = ('0' + mm).slice(-2); 
-       m = ('0' + m).slice(-2); 
-        s = ('0' + s).slice(-2);
-        ms = ('0' + ms).slice(-2);
+    　　　//桁数を合わせるため、０で埋めていく
+     　　  mm = ('0' + mm).slice(-2); 
+     　　  m = ('0' + m).slice(-2); 
+     　　  s = ('0' + s).slice(-2);
+     　　  ms = ('0' + ms).slice(-2);
       
-       timer.textContent = mm + ':'+ m + ':' + s + ':' + ms;
-   }
+      　　 timer.textContent = mm + ':'+ m + ':' + s + ':' + ms;
+　　   }
     
     
     
      //再帰的に使える用の関数
-    function countUp(){
+    　function countUp(){
 
         //timerId変数はsetTimeoutの返り値になるので代入する
         timerId = setTimeout(function(){
 
-            //経過時刻は現在時刻をミリ秒で示すDate.now()からstartを押した時の時刻(startTime)を引く
-            elapsedTime = Date.now() - startTime + timeToadd;
+          //経過時刻は現在時刻をミリ秒で示すDate.now()からstartを押した時の時刻(startTime)を引く
+           elapsedTime = Date.now() - startTime + timeToadd;
             
-            updateTimetText()
+           updateTimetText()
 
-            //countUp関数自身を呼ぶことで10ミリ秒毎に以下の計算を始める
-            countUp();
+          //countUp関数自身を呼ぶことで10ミリ秒毎に以下の計算を始める
+           countUp();
 
-        //1秒以下の時間を表示するために10ミリ秒後に始めるよう宣言
-        },10);
-    }
+        　//1秒以下の時間を表示するために10ミリ秒後に始めるよう宣言
+        　　},10);
+    　}
     
     
-    //startボタンにクリック時の動作
-    start.addEventListener('click',function(){
+   　 //startボタンにクリック時の動作
+    　　start.addEventListener('click',function(){
 
-        //在時刻を示すDate.nowを代入
-        startTime = Date.now();
+        　//在時刻を示すDate.nowを代入
+       　 startTime = Date.now();
 
-        //再帰的に使えるように関数を作る
-        countUp();
-    });
+        　//再帰的に使えるように関数を作る
+       　 countUp();
+  　  });
 
-    //stopボタンにクリック
-    stop.addEventListener('click',function(){
+   　 //stopボタンにクリック
+    　stop.addEventListener('click',function(){
 
-        //タイマーを止めるにはclearTimeoutを使う。clearTimeoutの引数に渡すためのタイマーのidが必要
-       clearTimeout(timerId);
+        　//タイマーを止めるにはclearTimeoutを使う。clearTimeoutの引数に渡すためのタイマーのidが必要
+      　　 clearTimeout(timerId);
 
 
-        //タイマーに表示される時間elapsedTimeが現在時刻かたスタートボタンを押した時刻を引いたもの。
-        //タイマーを再開させたら0になってしまう。
-        //それを回避するためには過去のスタート時間からストップ時間までの経過時間を足してあげなければならない。
-       //Date.now()=ストップを押した時間、startTime=過去にスタートを押した値
-       timeToadd += Date.now() - startTime;
-    });
+        　//タイマーに表示される時間elapsedTimeが現在時刻かたスタートボタンを押した時刻を引いたもの。
+        　//タイマーを再開させたら0になってしまう。
+       　 //それを回避するためには過去のスタート時間からストップ時間までの経過時間を足してあげなければならない。
+      　　//Date.now()=ストップを押した時間、startTime=過去にスタートを押した値
+      　　 timeToadd += Date.now() - startTime;
+   　 });
 
-    //resetボタンにクリック時
-    reset.addEventListener('click',function(){
+   　 //resetボタンにクリック時
+    　reset.addEventListener('click',function(){
 
-        //経過時刻を更新するための変数elapsedTimeを0
-        
-        elapsedTime = 0;
+       　 //経過時刻を更新するための変数elapsedTimeを0
+        　  elapsedTime = 0;
 
-        //リセット時に0に初期化したいのでリセットを押した際に0を代入してあげる
-        timeToadd = 0;
+        　//リセット時に0に初期化したいのでリセットを押した際に0を代入してあげる
+       　　 timeToadd = 0;
 
-        //updateTimetTextで0になったタイムを表示
-        updateTimetText();
+        　//updateTimetTextで0になったタイムを表示
+        　　updateTimetText();
 
-    });
+   　 });
+
 })();
